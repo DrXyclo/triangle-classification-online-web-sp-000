a6e6913c159a0cb1108d3896610d725e936dffb0
Dr. XyClo <sdcarmody@gmail.com>
2019-11-17 16:16:38 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,6 +9,14 @@ class Triangle
     elsif side1 != side2 && side2 != side3 && side1 != side3
       self.kind = :scalene
     end
+    
+    
+  class PartnerError < StandardError
+    
+    def message
+    "you must give the get_married method an argument of an instance of the person class!"
+    end
+  end 
   end
   
 #  def kind(side1, side2, side3)
