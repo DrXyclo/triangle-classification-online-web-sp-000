e626fc0ce604cd79a2503bad9e035d503de6486f
Dr. XyClo <sdcarmody@gmail.com>
2019-11-17 16:13:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,6 +6,8 @@ class Triangle
       self.kind = :equilateral
     elsif side1 == side2 || side2 == side3 || side1 == side3
       self.kind = :isosceles
+      elsif side1 != side2 && side2 != side3 && side1 != side3
+      self.kind = :scalene
     end
   end
   
